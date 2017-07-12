47631ebd5e2fc618b5c75b8d87c008df93bad0eb
Michael Dekker <git@michaeldekker.nl>
2017-07-12 20:21:11 +0200

    Maintenance update

--- classes/InfoBlock.php
+++ classes/InfoBlock.php
@@ -1,49 +1,56 @@
 <?php
-/*
-* 2007-2016 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2016 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+/**
+ * 2007-2016 PrestaShop
+ *
+ * thirty bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
+ * Copyright (C) 2017 thirty bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    thirty bees <modules@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017 thirty bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * PrestaShop is an internationally registered trademark & property of PrestaShop SA
+ */
 
+if (!defined('_TB_VERSION_')) {
+    exit;
+}
+
+/**
+ * Class InfoBlock
+ */
 class InfoBlock extends ObjectModel
 {
-	public $id;
-	
-	public $id_shop;
-	
-	public $text;
-
-	/**
-	 * @see ObjectModel::$definition
-	 */
-	public static $definition = array(
-		'table' => 'info',
-		'primary' => 'id_info',
-		'multilang' => true,
-		'fields' => array(
-			'id_shop' =>			array('type' => self::TYPE_NOTHING, 'validate' => 'isUnsignedId'),
-			// Lang fields
-			'text' =>				array('type' => self::TYPE_HTML, 'lang' => true, 'validate' => 'isCleanHtml', 'required' => true),
-		)
-	);
-
+    // @codingStandardsIgnoreStart
+    /**
+     * @see ObjectModel::$definition
+     */
+    public static $definition = [
+        'table'     => 'info',
+        'primary'   => 'id_info',
+        'multilang' => true,
+        'fields'    => [
+            'id_shop' => ['type' => self::TYPE_INT,  'validate' => 'isUnsignedId'],
+            // Lang fields
+            'text'    => ['type' => self::TYPE_HTML, 'lang' => true, 'validate' => 'isCleanHtml', 'required' => true],
+        ],
+    ];
+    /** @var int $id */
+    public $id;
+    /** @var int $id_shop */
+    public $id_shop;
+    /** @var string $text */
+    public $text;
+    // @codingStandardsIgnoreEnd
 }
