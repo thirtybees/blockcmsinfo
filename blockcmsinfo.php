943218e3484ea460dfe0684c27e6c2c1fdf72c48
Michael Dekker <git@michaeldekker.nl>
2017-10-15 23:09:16 +0200

    Bump to 2.0.3

--- blockcmsinfo.php
+++ blockcmsinfo.php
@@ -48,7 +48,7 @@ class Blockcmsinfo extends Module
     {
         $this->name = 'blockcmsinfo';
         $this->tab = 'front_office_features';
-        $this->version = '2.0.2';
+        $this->version = '2.0.3';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
         $this->need_instance = 0;
