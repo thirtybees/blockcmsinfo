8b6f0ea6f194602738f60b89a15f6b99a5031d02
Markus Hitter <mah@jump-ing.de>
2019-05-04 00:37:21 +0200

    Add constructor entry 'tb_min_version'.
    This is a requirement introduced recently by the validation script.

--- blockcmsinfo.php
+++ blockcmsinfo.php
@@ -58,6 +58,7 @@ class Blockcmsinfo extends Module
         $this->displayName = $this->l('Block CMS Info');
         $this->description = $this->l('Adds custom information blocks in your store.');
         $this->tb_versions_compliancy = '> 1.0.0';
+        $this->tb_min_version = '1.0.0';
     }
 
     /**
